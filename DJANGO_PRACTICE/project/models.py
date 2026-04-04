751a83bec98b65c6a8e515309e6ad5aba3a841b5
umeshkrbatra <umeshkrbatra008@gmail.com>
2026-04-04 15:31:28 +0530

    tailwind

--- DJANGO_PRACTICE/project/models.py
+++ DJANGO_PRACTICE/project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ProjectTypes(models.Model):
+    PROJECT_TYPE = [
+        ('RP','Research Projects'),
+        ('DP','Development Projects'),
+        ('MP','Maintenance Projects'),
+        ('EP','Exploratory Projects'),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=PROJECT_TYPE)
+
+    def __str__(self):
+        return self.name
--- DJANGO_PRACTICE/project/views.py
+++ DJANGO_PRACTICE/project/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ProjectTypes
 # Create your views here.
 
 def all_projects(request):
-    return render(request,'project/index.html')
+    projects = ProjectTypes.objects.all()
+    return render(request,'project/index.html', {'projects':projects})
